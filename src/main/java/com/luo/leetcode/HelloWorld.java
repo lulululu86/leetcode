1d75b212b6896b57ccc7524a289ea35307dbf888
luo zuo <luozuo@luos-MacBook-Pro.local>
2018-04-05 13:27:19 -0400

    testCommit

---
+++ src/main/java/com/luo/leetcode/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.luo.leetcode;
+
+public class HelloWorld {
+    private String msg;
+    public void printMessage(){
+        msg = "hello world!";
+        System.out.println(msg);
+    }
+}
