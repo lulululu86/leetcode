668ac16a68d1ebce7bdd66b5d33622cba133a382
luo zuo <luozuo@luos-MacBook-Pro.local>
2018-04-10 13:27:01 -0400

    Interval

---
+++ src/main/java/com/luo/leetcode/Interval.java
@@ -0,0 +1,10 @@
+package com.luo.leetcode;
+
+public class Interval {
+    int start;
+    int end;
+    public Interval(int start,int end){
+        this.start = start;
+        this.end = end;
+    }
+}
